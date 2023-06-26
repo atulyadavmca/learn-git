71bebe7644f528233826de2f2aeb90e366a4c4f3
atulyadavmca <atulyadavmca@gmail.com>
2023-06-26 12:43:58 +0530

    second commit

---
+++ Diff.java
@@ -0,0 +1,10 @@
+public class Test{
+public static static void main(String args[]){
+    int a=10;
+    int b=20;
+    int c = a+b;
+    System.out.println("printing sum"+c);
+     System.out.println("atul kumar yadav");
+
+    }
+}
